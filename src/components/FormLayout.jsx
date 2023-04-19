7ff0fde5e11077f59a6955ffe93673c0a501e6c7
Sofia <sraissiguier@gmail.com>
2023-04-19 15:06:55 -0300

    customize form component

--- src/components/FormLayout.jsx
+++ src/components/FormLayout.jsx
@@ -12,20 +12,30 @@
   }
   ```
 */
-import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
+import { CheckBadgeIcon, PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
 
 export default function FormLayout() {
   return (
     <form>
       <div className="space-y-12">
-        <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">Profile</h2>
-          <p className="mt-1 text-sm leading-6 text-gray-600">
-            This information will be displayed publicly so be careful what you share.
-          </p>
+        <div>
+          <div className="flex items-center justify-between">
+            <h2 className="text-base font-semibold leading-7 text-gray-900">Certificado de aporte</h2>
+            <div className="flex items-center">
+              <button className="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 mr-4">
+                Revisar
+              </button>
+              <button className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-indigo-600 hover:bg-indigo-200 hover:text-gray-700 inline-flex items-center">
+                <CheckBadgeIcon className="h-5 w-5 mr-1.5 text-white" aria-hidden="true"/>Aprobar
+              </button>
+            </div>
+          </div>
+            <p className="mt-1 text-sm leading-6 text-gray-500 border-b border-gray-900/10 pb-6">
+              Version 1
+            </p>
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <div className="sm:col-span-4">
+            {/* <div className="sm:col-span-4">
               <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
                 Username
               </label>
@@ -42,9 +52,9 @@ export default function FormLayout() {
                   />
                 </div>
               </div>
-            </div>
+            </div> */}
 
-            <div className="col-span-full">
+            {/* <div className="col-span-full">
               <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
                 About
               </label>
@@ -58,9 +68,9 @@ export default function FormLayout() {
                 />
               </div>
               <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
-            </div>
+            </div> */}
 
-            <div className="col-span-full">
+            {/* <div className="col-span-full">
               <label htmlFor="photo" className="block text-sm font-medium leading-6 text-gray-900">
                 Photo
               </label>
@@ -73,11 +83,11 @@ export default function FormLayout() {
                   Change
                 </button>
               </div>
-            </div>
+            </div> */}
 
             <div className="col-span-full">
               <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
-                Cover photo
+                Comprobante de pago
               </label>
               <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
                 <div className="text-center">
@@ -92,21 +102,21 @@ export default function FormLayout() {
                     </label>
                     <p className="pl-1">or drag and drop</p>
                   </div>
-                  <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+                  {/* <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p> */}
                 </div>
               </div>
             </div>
           </div>
         </div>
 
-        <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
-          <p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
+        <div>
+          {/* <h2 className="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
+          <p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p> */}
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <div className="sm:col-span-3">
+            <div className="sm:col-span-6">
               <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
-                First name
+                Fecha de pago
               </label>
               <div className="mt-2">
                 <input
@@ -119,7 +129,7 @@ export default function FormLayout() {
               </div>
             </div>
 
-            <div className="sm:col-span-3">
+            {/* <div className="sm:col-span-3">
               <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
                 Last name
               </label>
@@ -132,9 +142,9 @@ export default function FormLayout() {
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
-            </div>
+            </div> */}
 
-            <div className="sm:col-span-4">
+            {/* <div className="sm:col-span-4">
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                 Email address
               </label>
@@ -147,9 +157,9 @@ export default function FormLayout() {
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
-            </div>
+            </div> */}
 
-            <div className="sm:col-span-3">
+            {/* <div className="sm:col-span-3">
               <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
                 Country
               </label>
@@ -165,9 +175,9 @@ export default function FormLayout() {
                   <option>Mexico</option>
                 </select>
               </div>
-            </div>
+            </div> */}
 
-            <div className="col-span-full">
+            {/* <div className="col-span-full">
               <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-gray-900">
                 Street address
               </label>
@@ -180,9 +190,9 @@ export default function FormLayout() {
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
-            </div>
+            </div> */}
 
-            <div className="sm:col-span-2 sm:col-start-1">
+            {/* <div className="sm:col-span-2 sm:col-start-1">
               <label htmlFor="city" className="block text-sm font-medium leading-6 text-gray-900">
                 City
               </label>
@@ -195,9 +205,9 @@ export default function FormLayout() {
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
-            </div>
+            </div> */}
 
-            <div className="sm:col-span-2">
+            {/* <div className="sm:col-span-2">
               <label htmlFor="region" className="block text-sm font-medium leading-6 text-gray-900">
                 State / Province
               </label>
@@ -210,9 +220,9 @@ export default function FormLayout() {
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
-            </div>
+            </div> */}
 
-            <div className="sm:col-span-2">
+            {/* <div className="sm:col-span-2">
               <label htmlFor="postal-code" className="block text-sm font-medium leading-6 text-gray-900">
                 ZIP / Postal code
               </label>
@@ -225,11 +235,11 @@ export default function FormLayout() {
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
 
-        <div className="border-b border-gray-900/10 pb-12">
+        {/* <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base font-semibold leading-7 text-gray-900">Notifications</h2>
           <p className="mt-1 text-sm leading-6 text-gray-600">
             We'll always let you know about important changes, but you pick what else you want to hear about.
@@ -329,10 +339,10 @@ export default function FormLayout() {
               </div>
             </fieldset>
           </div>
-        </div>
+        </div> */}
       </div>
 
-      <div className="mt-6 flex items-center justify-end gap-x-6">
+      {/* <div className="mt-6 flex items-center justify-end gap-x-6">
         <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
           Cancel
         </button>
@@ -342,7 +352,7 @@ export default function FormLayout() {
         >
           Save
         </button>
-      </div>
+      </div> */}
     </form>
   )
 }
